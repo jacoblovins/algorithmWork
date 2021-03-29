c32f3e5eef84064f143e6ba1e72a177f4d212a3f
jacoblovins <lovins.jacob@yahoo.com>
2021-03-29 09:24:03 -0400

    final mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -23,3 +23,19 @@ const merge = (arr1, arr2) => {
     }
     return results;
  }
+
+//  recursive merseSort function
+
+ function mergeSort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, right);
+
+}
+
+console.log(mergeSort([10,24,76,73,72,1,9]))
