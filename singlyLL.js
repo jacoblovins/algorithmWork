5d028cb813882f0dd76e45e07601821a07eb6069
jacoblovins <lovins.jacob@yahoo.com>
2021-03-30 14:10:47 -0400

    SLL push method added

--- singlyLL.js
+++ singlyLL.js
@@ -9,10 +9,33 @@ class Node {
     }
 }
 
-const first = new Node("Hi");
-first.next = new Node("there");
-first.next.next = new Node("how");
-first.next.next.next = new Node("are");
-first.next.next.next.next = new Node("you");
+// const first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
 
-console.log(first)
+// console.log(first)
+
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val){
+        const newNode = new Node(val);
+        // if the list is empty
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++
+        return this;
+    }
+
+}
