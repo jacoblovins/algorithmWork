7d174bd6435a6b5399bf06d2c3d83a8184839748
jacoblovins <lovins.jacob@yahoo.com>
2021-03-31 14:06:29 -0400

    setting up quick sort and explanation

---
+++ quickSort.js
@@ -0,0 +1,5 @@
+// solve through recursion
+// like merge sort, split to arrays of 1 or 0
+// works by selecting one element called the pivot and finding the index of where the pivot should end up in the sorted array
+// once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot
+
