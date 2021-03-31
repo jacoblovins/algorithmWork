efec21096c9ff4e2eb05427481c8a2eb0bc1c541
jacoblovins <lovins.jacob@yahoo.com>
2021-03-31 14:08:54 -0400

    quick sort function finalized

--- quickSort.js
+++ quickSort.js
@@ -22,3 +22,16 @@ function pivot(arr, start=0, end=arr.length+1){
     swap(arr, start, swapIdx);
     return swapIdx;
 }
+
+function quickSort(arr, left=0, right=arr.length-1){
+    if(left < right){
+        let pivotIndex = pivot(arr, left, right);
+        //left
+        quickSort(arr, left, pivotIndex-1);
+        //right
+        quickSort(arr, pivotIndex+1, right);
+    }
+    return arr;
+}
+
+console.log(quickSort([4,6,9,1,2,5,3]));
