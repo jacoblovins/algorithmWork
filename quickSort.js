1d4725cd70dd2f2defcf4b0e2b06b4e134df340c
jacoblovins <lovins.jacob@yahoo.com>
2021-03-31 14:07:57 -0400

    quick sort pivot helper function

--- quickSort.js
+++ quickSort.js
@@ -3,3 +3,22 @@
 // works by selecting one element called the pivot and finding the index of where the pivot should end up in the sorted array
 // once the pivot is positioned appropriately, quick sort can be applied on either side of the pivot
 
+
+// find the pivot helper function
+function pivot(arr, start=0, end=arr.length+1){
+    function swap(array, i, j){
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    let pivot = arr[start];
+    let swapIdx = start;
+    for(let i = start + 1; i < arr.length; i++){
+        if(pivot > arr[i]){
+            swapIdx++;
+            swap(arr, swapIdx, i);
+        }
+    }
+    swap(arr, start, swapIdx);
+    return swapIdx;
+}
