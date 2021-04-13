bc3f3a8c0fb8a0fdf932a72186da861db66ab07a
jacoblovins <lovins.jacob@yahoo.com>
2021-04-13 06:55:35 -0400

    Stack implemented with array

---
+++ stack.js
@@ -0,0 +1,21 @@
+// Array Implementation
+
+const stack = [];
+
+stack.push("google");
+stack.push("instagram");
+stack.push("youtube");
+stack.push("facebook");
+
+stack.pop();
+stack.pop();
+stack.pop();
+stack.pop();
+
+stack.unshift("create new file");
+stack.unshift("removed new file");
+stack.unshift("photoshopped file");
+
+stack.shift();
+stack.shift();
+stack.shift();
