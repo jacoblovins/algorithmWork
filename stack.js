a8ae18635b63c487ab311670a387bdbe214c9678
jacoblovins <lovins.jacob@yahoo.com>
2021-04-13 07:26:36 -0400

    Stack with a linked list implemented

--- stack.js
+++ stack.js
@@ -1,21 +1,72 @@
-// Array Implementation
+// // Array Implementation
 
-const stack = [];
+// const stack = [];
 
-stack.push("google");
-stack.push("instagram");
-stack.push("youtube");
-stack.push("facebook");
+// stack.push("google");
+// stack.push("instagram");
+// stack.push("youtube");
+// stack.push("facebook");
 
-stack.pop();
-stack.pop();
-stack.pop();
-stack.pop();
+// stack.pop();
+// stack.pop();
+// stack.pop();
+// stack.pop();
 
-stack.unshift("create new file");
-stack.unshift("removed new file");
-stack.unshift("photoshopped file");
+// stack.unshift("create new file");
+// stack.unshift("removed new file");
+// stack.unshift("photoshopped file");
 
-stack.shift();
-stack.shift();
-stack.shift();
+// stack.shift();
+// stack.shift();
+// stack.shift();
+
+
+
+// linked list implementation
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val){
+        let newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            let oldHead = this.head
+            this.head = newNode;
+            this.head.next = oldHead;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop(){
+        let oldHead = this.head;
+        this.head = oldHead.next;
+        oldHead.next = null;
+        this.length--;
+        return oldHead;
+    }
+}
+
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+console.log(stack.pop());
+
+console.log(stack);
