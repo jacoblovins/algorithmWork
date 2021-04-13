d144cdad6cb9df820309f2c9aea2665ebbdcf5b1
jacoblovins <lovins.jacob@yahoo.com>
2021-04-13 08:23:12 -0400

    Queue linked list implementation

--- queue.js
+++ queue.js
@@ -2,86 +2,86 @@
 
 // // Array implementation- not good on time complexity
 
-const q = [];
+// const q = [];
 
-q.push(1);
-q.push(2);
-q.push(3);
+// q.push(1);
+// q.push(2);
+// q.push(3);
 
-q.shift();
-q.shift();
-q.shift();
+// q.shift();
+// q.shift();
+// q.shift();
 
-// or
+// // or
 
-q.unshift(1);
-q.unshift(2);
-q.unshift(3);
+// q.unshift(1);
+// q.unshift(2);
+// q.unshift(3);
 
-q.pop();
-q.pop();
-q.pop();
+// q.pop();
+// q.pop();
+// q.pop();
 
 
-// // Linked list
+// Linked list
 
-// class Node{
-//     constructor(val){
-//         this.val = val;
-//         this.next = null;
-//     }
-// }
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
 
-// class Queue{
-//     constructor(){
-//         this.head = null;
-//         this.tail = null;
-//         this.length = 0;
-//     }
+class Queue{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 
-//     enqueue(val){
-//         const newNode = new Node(val);
-//         if(this.length === 0){
-//             this.head = newNode;
-//             this.tail = newNode;
-//         } else {
-//             this.tail.next = newNode;
-//             this.tail = newNode;
-//         }
-//         this.length++;
-//         return this.length;
-//     }
+    enqueue(val){
+        const newNode = new Node(val);
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this.length;
+    }
 
-//     dequeue(){
+    dequeue(){
 
-//         if(this.length === 0) return null;
-//         const oldHead = this.head;
-//         if(this.length === 1){
-//             this.head = null;
-//             this.tail = null;
-//         } else {
-//             this.head = oldHead.next;
-//         }
-//         oldHead.next = null;
-//         this.length--;
-//         return oldHead;
+        if(this.length === 0) return null;
+        const oldHead = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = oldHead.next;
+        }
+        oldHead.next = null;
+        this.length--;
+        return oldHead;
 
-//     }
-// }
+    }
+}
 
-// const q = new Queue();
-// q.enqueue(1);
-// q.enqueue(2);
-// q.enqueue(3);
-// q.enqueue(4);
+const q = new Queue();
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+q.enqueue(4);
 
-// q.dequeue();
-// q.dequeue();
-// q.dequeue();
-// q.dequeue();
-// q.enqueue(1);
-// q.enqueue(2);
+q.dequeue();
+q.dequeue();
+q.dequeue();
+q.dequeue();
+q.enqueue(1);
+q.enqueue(2);
 
-// console.log(q)
+console.log(q)
 
 
