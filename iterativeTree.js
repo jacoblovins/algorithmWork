33904914f6922f52cbb0808ba0ce3b6dbf804754
jacoblovins <lovins.jacob@yahoo.com>
2021-04-14 13:43:04 -0400

    Binary Search Tree iterative search pseudo

--- iterativeTree.js
+++ iterativeTree.js
@@ -65,6 +65,18 @@ class BinarySearchTree{
 
     // find()
 
+    // Check if there is a root, if not we are done searching
+    // If there is a root, check if the value of the new node is the value we are looking for. If we found it, we are done
+    // If not, check to see if the value is greater than or less than the value of the root
+    // If it is greater:
+        // Check to see if there is a node to the right
+            // If there is, move to that node and repeat these steps
+            // If there is not, we are done searching
+    // If it is less:
+        // Check to see if there is a node to the left
+            // If there is, move to that node and repeat these steps
+            // If ther is not, we are done searching
+
     find(val){
         if(!this.root){
             return false;
