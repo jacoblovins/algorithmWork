b0744603f605574afafc4ac13cfebdff1b3ec7be
jacoblovins <lovins.jacob@yahoo.com>
2021-04-14 13:51:57 -0400

    Binary Search Tree iterative find refactored

--- iterativeTree.js
+++ iterativeTree.js
@@ -1,5 +1,5 @@
-class Node{
-    constructor(val){
+class Node {
+    constructor(val) {
         this.val = val;
         this.left = null;
         this.right = null;
@@ -7,8 +7,8 @@ class Node{
     }
 }
 
-class BinarySearchTree{
-    constructor(){
+class BinarySearchTree {
+    constructor() {
         this.root = null;
     }
 
@@ -16,43 +16,43 @@ class BinarySearchTree{
 
     // Create a new node
     // Starting at the root:
-        // Check if there is a root, if not, the root now becomes that new node.
-        // If there is a root, check if the value of the new node is greater than or less than the value of the root.
-        // If it is greater:
-            // Check to see if there is a node to the right
-                // If there is, move to that node and repeat these steps
-                // If there is not, add that node as the right property
-        // If it is less:
-            // Check to see if there is a node to the left
-                // If there is , move to that node and repeat these steps
-                // If there is not, add that node as the left property
+    // Check if there is a root, if not, the root now becomes that new node.
+    // If there is a root, check if the value of the new node is greater than or less than the value of the root.
+    // If it is greater:
+    // Check to see if there is a node to the right
+    // If there is, move to that node and repeat these steps
+    // If there is not, add that node as the right property
+    // If it is less:
+    // Check to see if there is a node to the left
+    // If there is , move to that node and repeat these steps
+    // If there is not, add that node as the left property
 
-    insert(val){
+    insert(val) {
         const newNode = new Node(val);
 
-        if(this.root === null){
+        if (this.root === null) {
             this.root = newNode;
             return this;
         } else {
             let current = this.root;
-            if(val === current.val){
+            if (val === current.val) {
                 current.count++;
                 return this;
             }
-            while(true){
-                if(val === current.val){
+            while (true) {
+                if (val === current.val) {
                     current.count++;
                     return this;
                 }
-                if(val < current.val){
-                    if(current.left === null){
+                if (val < current.val) {
+                    if (current.left === null) {
                         current.left = newNode;
                         return this;
                     } else {
                         current = current.left;
                     }
-                } else if(val > current.val){
-                    if(current.right === null){
+                } else if (val > current.val) {
+                    if (current.right === null) {
                         current.right = newNode;
                         return this;
                     } else {
@@ -69,31 +69,27 @@ class BinarySearchTree{
     // If there is a root, check if the value of the new node is the value we are looking for. If we found it, we are done
     // If not, check to see if the value is greater than or less than the value of the root
     // If it is greater:
-        // Check to see if there is a node to the right
-            // If there is, move to that node and repeat these steps
-            // If there is not, we are done searching
+    // Check to see if there is a node to the right
+    // If there is, move to that node and repeat these steps
+    // If there is not, we are done searching
     // If it is less:
-        // Check to see if there is a node to the left
-            // If there is, move to that node and repeat these steps
-            // If ther is not, we are done searching
+    // Check to see if there is a node to the left
+    // If there is, move to that node and repeat these steps
+    // If ther is not, we are done searching
 
-    find(val){
-        if(!this.root){
-            return false;
-        } else {
-            let current = this.root;
-
-            while(current){
-                if(val === current.val){
-                    return true;
-                } else if(val < current.val){
-                    current = current.left;
-                } else {
-                    current = current.right;
-                }
+    find(val) {
+        if (!this.root) return false;
+        let current = this.root;
+        while (current) {
+            if (val === current.val) {
+                return true;
+            } else if (val < current.val) {
+                current = current.left;
+            } else {
+                current = current.right;
             }
-            return false;
         }
+        return false;
     }
 }
 
@@ -109,7 +105,7 @@ tree.insert(3);
 tree.insert(10);
 tree.insert(12);
 
-console.log(tree.find(22));
+console.log(tree.find(10));
 
 // console.log(tree);
 
