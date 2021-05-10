d7cfe0108f5bac803915ee957927fbf3d35d0fbe
jacoblovins <lovins.jacob@yahoo.com>
2021-05-10 08:17:37 -0400

    Graph adding edges

--- graph.js
+++ graph.js
@@ -10,12 +10,12 @@ class Graph {
         this.adjacencyList[vertex] = [];
     }
 
-    // // find vertex1 in the list and push vertex2 to it's array
-    // // find vertex2 and push vertex1 go it's array
-    // addEdge(v1, v2){
-    //     this.adjacencyList[v1].push(v2);
-    //     this.adjacencyList[v2].push(v1);
-    // }
+    // find vertex1 in the list and push vertex2 to it's array
+    // find vertex2 and push vertex1 go it's array
+    addEdge(v1, v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
 
     // removeEdge(vertex1, vertex2){
     //     this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
@@ -37,9 +37,9 @@ g.addVertex("hello");
 g.addVertex("goodbye");
 g.addVertex("something");
 g.addVertex("another");
-// g.addEdge("hello", "goodbye")
-// g.addEdge("hello", "another")
-// g.addEdge("goodbye", "something")
+g.addEdge("hello", "goodbye")
+g.addEdge("hello", "another")
+g.addEdge("goodbye", "something")
 
 // g.removeEdge("hello", "goodbye")
 
