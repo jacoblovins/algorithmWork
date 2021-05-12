8f0ee7334ee2621b15f2769f6613bfe0cfb05221
jacoblovins <lovins.jacob@yahoo.com>
2021-05-12 07:17:04 -0400

    Graph BFS initial setup and pseudo

--- graph.js
+++ graph.js
@@ -101,6 +101,23 @@ class Graph {
         }
         return result;
     }
+
+
+    // Function should accept a starting vertex
+    // Create a queue and place the starting vertex in it
+    // Create an array to store the nodes visited
+    // Create an object to store nodes visited
+    // Mark the starting vertex as visited
+    // Loop as long as there is anything in the queue
+        // Remove the first vertex from the queue and push it into the array that stores nodes visited
+        // Loop over each vertex in the adjacency list for the vertex you are visiting
+            // If it is not inside the object that stores nodes visited, mark it as visited and enqueue that vertex
+    // Return the array of visited nodes
+
+    breadthFirst(start){
+
+    }
+
 }
 
 
