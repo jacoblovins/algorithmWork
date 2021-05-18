54328ca9f17e79e84537dfe16fa0ef29cef25e2a
jacoblovins <lovins.jacob@yahoo.com>
2021-05-18 10:16:44 -0400

    Dijkstra Priority Queue added

--- dijkstra.js
+++ dijkstra.js
@@ -16,6 +16,31 @@ class WeightedGraph{
 
 }
 
+class PriorityQueue{
+    constructor(){
+        this.values = [];
+    }
+    enqueue(val, priority){
+        this.values.push({val, priority});
+        this.sort();
+    };
+    dequeue(){
+        return this.values.shift();
+    };
+    sort(){
+        this.values.sort((a, b) => a.priority - b.priority);
+    };
+
+}
+
+// Every time we look to visit a new node, we pick the node with the smallest known distance to visit first
+// Once we've moved to the node we're going to visit, we look at each of its neighbors
+// For each neighboring node, we calculate the distance by summing the total edges that lead to the node we're checking from the starting node
+// If the new total distance to a node is less than the previous total, we store the new shorter distance for that node.
+
+
+
+
 const graph = new WeightedGraph();
 
 graph.addVertex("A");
