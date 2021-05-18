29d79e4f739ee2e1394a1760b1efa34047a632a0
jacoblovins <lovins.jacob@yahoo.com>
2021-05-18 11:18:55 -0400

    Dijkstra Method upgraded with a heap

--- dijkstra.js
+++ dijkstra.js
@@ -22,22 +22,74 @@
         // Enqueue the vertex with the total distance from the start node\
 
         
-        
-class PriorityQueue{
+
+class PriorityQueue {
     constructor(){
         this.values = [];
     }
-    enqueue(val, priority){
-        this.values.push({val, priority});
-        this.sort();
-    };
+    enqueue(val, priority) {
+        let newNode = new Node(val, priority)
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length -1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIndex = Math.floor((idx - 1) / 2);
+            let parent = this.values[parentIndex];
+            if(element.priority >= parent.priority) break;
+            this.values[parentIndex] = element;
+            this.values[idx] = parent;
+            idx = parentIndex;
+        }
+    }
+
     dequeue(){
-        return this.values.shift();
-    };
-    sort(){
-        this.values.sort((a, b) => a.priority - b.priority);
-    };
-    
+        const min = this.values[0];
+        let end = this.values.pop();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+
+        while(true){
+            let leftChildIdx = (2 * idx) + 1;
+            let rightChildIdx = (2 * idx) + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild.priority < element.priority){
+                    swap = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if((swap === null && rightChild.priority < element.priority) || (swap !== null && rightChild.priority < leftChild.priority)){
+                    swap = rightChildIdx;
+                }
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
+}
+
+class Node {
+    constructor(val, priority){
+        this.val = val;
+        this.priority = priority
+    }
 }
         
          
@@ -108,6 +160,8 @@ class WeightedGraph{
     }
 }
 
+
+
 const graph = new WeightedGraph();
 
 graph.addVertex("A");
@@ -127,17 +181,3 @@ graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
 
 console.log(graph.Dijkstra("A", "E"));
-
-
-
-
-
-/*
-    Instead of just an array, we need to store the vertex and the weight of the edge so we 
-    will store objects with both properties inside an arrray.
-
-    {
-        "A": [{node: "B", weight: 10}]
-    }
-
-*/
