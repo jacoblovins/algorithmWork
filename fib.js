9f1e810c2b21932f7a2bc3d6a4324d599d7a4196
jacoblovins <lovins.jacob@yahoo.com>
2021-05-19 11:49:57 -0400

    Dynamic Programming Tabulated Fibonacci

--- fib.js
+++ fib.js
@@ -1,4 +1,5 @@
 // Dynamic Recursive fibonacci sequence using Memoization
+
 function fib_memo(n, memo=[]){
     if(memo[n] !== undefined) return memo[n];
     if(n <= 2) return 1;
@@ -7,4 +8,17 @@ function fib_memo(n, memo=[]){
     return res;
 }
 
-console.log(fib_memo(6));
+console.log(fib_memo(10));
+
+// Dynamic Tabulated fibonacci sequence
+
+function fib_table(n){
+    if(n <= 2) return 1;
+    let fibNums = [0, 1, 1];
+    for(let i = 3; i <= n; i++){
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+    return fibNums[n];
+}
+
+console.log(fib_table(10));
